c2d537e933f6aa4804250bcb08d313d112800801
Lei-Mutya <solielmutya@gmail.com>
2021-08-30 09:55:09 +0800

    updated the route path

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Contact from "./components/Contact";
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Visionmission from "./components/Visionmission";
-import Administration from "./components/Administration";
-import AdmissReq from "./components/AdmissReq";
-import AppForm from "./components/AppForm";
-import AppResult from "./components/AppResult";
-import Coursesoff from "./components/Coursesoff";
-import Sis from "./components/Sis";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+import Footer from './components/Footer';
+import Visionmission from './components/Visionmission';
+import Administration from './components/Administration';
+import AdmissReq from './components/AdmissReq';
+import AppForm from './components/AppForm';
+import AppResult from './components/AppResult';
+import Coursesoff from './components/Coursesoff';
+import Sis from './components/Sis';
 import AdminLog from './components/AdminLog';
 import AdminMod from './components/AdminMod';
 import AddStudent from './components/AddStudent';
@@ -28,22 +28,22 @@ function App() {
         <Navbar />
         <div className="container">
           <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/visionmission" component={Visionmission} />
-            <Route path="/administration" component={Administration} />
-            <Route path="/admissreq" component={AdmissReq} />
-            <Route path="/appform" component={AppForm} />
-            <Route path="/appresult" component={AppResult} />
-            <Route path="/coursesoff" component={Coursesoff} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/sis" component={Sis} />
-            <Route path="/adminlog" component={AdminLog}/>
-            <Route path="/adminmod" component={AdminMod}/>
-            <Route path="/addstudent" component={AddStudent}/>
-            <Route path="/applicantverify" component={ApplicantVerify}/>
-            <Route path="/studentlog" component={StudentLog}/>
-            <Route path="/studentmod" component={StudentMod}/>
+            <Route path="/test2" exact component={Home} />
+            <Route path="/test2/about" component={About} />
+            <Route path="/test2/visionmission" component={Visionmission} />
+            <Route path="/test2/administration" component={Administration} />
+            <Route path="/test2/admissreq" component={AdmissReq} />
+            <Route path="/test2/appform" component={AppForm} />
+            <Route path="/test2/appresult" component={AppResult} />
+            <Route path="/test2/coursesoff" component={Coursesoff} />
+            <Route path="/test2/contact" component={Contact} />
+            <Route path="/test2/sis" component={Sis} />
+            <Route path="/test2/adminlog" component={AdminLog} />
+            <Route path="/test2/adminmod" component={AdminMod} />
+            <Route path="/test2/addstudent" component={AddStudent} />
+            <Route path="/test2/applicantverify" component={ApplicantVerify} />
+            <Route path="/test2/studentlog" component={StudentLog} />
+            <Route path="/test2/studentmod" component={StudentMod} />
           </Switch>
         </div>
         <Footer />
